437e5c62e33031e66b2b55ff801a2fa3ccb44f42
opr-inzn <max.inzn@gmail.com>
2025-08-23 11:51:01 +0200

    Update Round Kerning to 5.py

--- Kerning/Round Kerning to 5.py
+++ Kerning/Round Kerning to 5.py
@@ -2,62 +2,100 @@
 # encoding: utf-8
 
 __doc__ = """
-Rounds the kerning values to the nearest multiple of 5.
-
-In addition, values smaller than MIN_VALUE are erased.
+Rounds the kerning values to units of 5.
+Original script by Wei Huang.
 """
 
-MIN_VALUE = 5
-
 from GlyphsApp import *
-font = Glyphs.font
+from vanilla import *
+
 
 def myround(x, base=5):
     return int(base * round(float(x) / base))
 
-printString = """"""
 
-for eachFontMaster in font.masters:
-    eachFontMasterId = eachFontMaster.id
-    to_be_removed = []
+class RoundKerningDialog(object):
+    def __init__(self):
+        self.w = Window((300, 140), "Round Kerning to 5")
 
-    for first, seconds in dict(font.kerning[eachFontMasterId]).items():
-        # normalize first side
-        if isinstance(first, int):
-            firstName = font.glyphForId_(first).name
-        else:
-            firstName = first
+        self.w.text = TextBox((15, 12, -15, 20), "Remove pairs smaller than:")
+        self.w.minValue = EditText(
+            (200, 10, -15, 22), "5", sizeStyle="regular"
+        )
 
-        for second, existingValue in dict(seconds).items():
-            # normalize second side
-            if isinstance(second, int):
-                secondName = font.glyphForId_(second).name
-            else:
-                secondName = second
+        self.w.runButton = Button(
+            (15, 60, -15, 30), "Round Kerning", callback=self.roundKerning
+        )
 
-            # round towards 5
-            value = myround(existingValue, 5)
+        self.w.open()
+        self.w.makeKey()
 
-            if abs(existingValue) < MIN_VALUE:
-                to_be_removed.append((firstName, secondName, existingValue))
-            elif existingValue != value:
-                font.setKerningForPair(eachFontMasterId, firstName, secondName, value)
-                printString += "%s\t%s: %s, %s, %s\n" % (
-                    value,
+    def roundKerning(self, sender):
+        try:
+            minValue = int(self.w.minValue.get())
+        except ValueError:
+            Message("Please enter a valid integer for MIN_VALUE.")
+            return
+
+        font = Glyphs.font
+        printString = ""
+
+        for eachFontMaster in font.masters:
+            eachFontMasterId = eachFontMaster.id
+            to_be_removed = []
+
+            for first, seconds in dict(font.kerning[eachFontMasterId]).items():
+                # normalize first side
+                if isinstance(first, int):
+                    firstName = font.glyphForId_(first).name
+                else:
+                    firstName = first
+
+                for second, existingValue in dict(seconds).items():
+                    # normalize second side
+                    if isinstance(second, int):
+                        secondName = font.glyphForId_(second).name
+                    else:
+                        secondName = second
+
+                    # round towards 5
+                    value = myround(existingValue, 5)
+
+                    if abs(existingValue) < minValue:
+                        to_be_removed.append(
+                            (firstName, secondName, existingValue)
+                        )
+                    elif existingValue != value:
+                        font.setKerningForPair(
+                            eachFontMasterId, firstName, secondName, value
+                        )
+                        printString += "%s\t%s: %s, %s, %s\n" % (
+                            value,
+                            eachFontMaster.name,
+                            firstName,
+                            secondName,
+                            existingValue,
+                        )
+
+            # remove too-small pairs
+            for firstName, secondName, existingValue in to_be_removed:
+                font.removeKerningForPair(
+                    eachFontMasterId, firstName, secondName
+                )
+                printString += "0\t%s: %s, %s, %s\n" % (
                     eachFontMaster.name,
                     firstName,
                     secondName,
                     existingValue,
                 )
 
-    # remove too-small pairs
-    for firstName, secondName, existingValue in to_be_removed:
-        font.removeKerningForPair(eachFontMasterId, firstName, secondName)
-        printString += "0\t%s: %s, %s, %s\n" % (
-            eachFontMaster.name,
-            firstName,
-            secondName,
-            existingValue,
+        print(printString)
+        Glyphs.showNotification(
+            "Kerning Rounded",
+            f"Kerning rounded to 5, pairs smaller than {minValue} removed.",
         )
 
-print(printString)
+        # ✅ Close dialog after running
+        self.w.close()
+
+RoundKerningDialog()
